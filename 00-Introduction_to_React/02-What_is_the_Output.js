36edef4f2d0634636be9846ae55d76bfea614eeb
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-18 19:59:21 +0530

    MCQ Around SPA vs MPA

---
+++ 00-Introduction_to_React/02-What_is_the_Output.js
@@ -0,0 +1,8 @@
+/*
+Which of the following is/are correct about SPA(Single page application)?
+
+Single-Page Applications have a more complex structure.         
+Single-Page Applications dont't require a full-page reload.                     //* Correct
+Single-Page Applications are unsuitable for search engine optimization.         //* Correct=
+Single-Page Applications are slower than multi-page applications
+*/
