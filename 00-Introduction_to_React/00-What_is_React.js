515379cce270dcb8b7ea20381927db6122887643
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-18 19:25:10 +0530

    Understand React and Benefits of React

---
+++ 00-Introduction_to_React/00-What_is_React.js
@@ -0,0 +1,36 @@
+/*
+    Library : Library is Predefine function which makes our work easier, 
+        fast and convienient
+
+*/
+
+/* 
+    React : React is a Javascript Library to build User Interface (UI).We Can 
+        Create User Interface Directly with Javascript but react offers more 
+        better way to build User Interface.
+
+        Official Documentation : "https://legacy.reactjs.org/"
+*/
+
+/*
+    History of React : React is Created in 2013 by "Jordan Walke" and Currently 
+        maintain by the META(Facebook) and community of Developers and V18 is 
+        the Latest.
+*/
+
+/*
+    Why React : 
+        - React is Composable : Any Complex task we can Devide into multiple smaller 
+                    components or smaller parts for reducing the Complexity of UI. 
+                    This helps us to core reusability. Components is Just like a
+                    Javascript function or something else of javascript code
+
+        - Build Single Page Applications : What is single Page Application, Single 
+                    Page Applications is the applications which has only one html 
+                    Document. It helps us to Prevent web page Reload again and again 
+                    instead of this changes made dynamically. That's is the reason 
+                    performance is very high of this applications. Simple Javascript 
+                    can able to create this single page applications but using simple 
+                    javascript it's very complex to create.
+
+*/
