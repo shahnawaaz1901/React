57b4822602f5cb69f3a98485e5705ab7680c2388
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-18 19:56:22 +0530

    Understand SPA vs MPA

---
+++ 00-Introduction_to_React/02-SPA_vs_MPA.js
@@ -0,0 +1,18 @@
+/*
+    Multipage Application : Multipage applications is traditional way to create 
+                applications where for every task a reload is happening and multiple 
+                html document is create for every single task. Or We can understand
+                multipage application those where reload is happening while user
+                interacts with the App. Multi Page Application is Search Engine 
+                Optimization Friendly where Search Engine Ranked each page. That's
+                why Amazon and Other E-Commerce applications uses MPA. In Multi Page
+                Application Post request is send to the server and Whole html document
+                server send to the client
+
+    Singlepage Application : Single Page application is the modern way to create
+                applications where a single html document is present and reload
+                is happening only once. And when ever user interacts with the
+                application changes made dynamically without reload web page again
+                and again using ajax request to the server for any information.
+                Against AJAX Request a JSON response is sends back to the client 
+*/
