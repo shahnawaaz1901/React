8430dc4f0c45b59983371a36662658da6f9b0e21
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-18 20:21:52 +0530

    Understand Declarative Behaviour of React

---
+++ 00-Introduction_to_React/03-How_React_Declarative.js
@@ -0,0 +1,8 @@
+/*
+    React is Declarative Library which describe what you want to do and how 
+    what you dont want to do. Means we tell browser to create a heading and 
+    browser will create heading for you just like we create heading in HTML
+    just writing h1 tag but this declarative not present in javascript, because
+    javascript is imperative language means we need to tell each and every 
+    step in javascript but this is not in case of react
+*/
---
+++ 00-Introduction_to_React/04-What_is_the_Output.js
@@ -0,0 +1,8 @@
+/*
+Identify the correct statement(s) describing React as a declarative language.
+
+React Describes rthe desired outcome, not the specific steps to achieve it.
+React allows for more efficient updates to the DOM
+React eleminates the need for the manual DOM manipulation.
+All of the Above.                                               //* Correct
+*/
