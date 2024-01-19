1fe14a480f9dc07a91e25c99f230f1a99266793e
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-19 18:23:39 +0530

    Create Element using Javascript

--- 00-Introduction_to_React/04-What_is_the_Output.js
+++ 00-Introduction_to_React/04-What_is_the_Output.js
@@ -1,8 +1,9 @@
 /*
 Identify the correct statement(s) describing React as a declarative language.
 
-React Describes rthe desired outcome, not the specific steps to achieve it.
-React allows for more efficient updates to the DOM
-React eleminates the need for the manual DOM manipulation.
-All of the Above.                                               //* Correct
+1. React Describes rthe desired outcome, not the specific steps 
+    to achieve it.
+2. React allows for more efficient updates to the DOM
+3. React eleminates the need for the manual DOM manipulation.
+4. All of the Above.                                               //* Correct
 */
---
+++ 00-Introduction_to_React/05-Create_Element_in_JavaScript.js
@@ -0,0 +1,5 @@
+const newElement = document.createElement("h2");
+newElement.textContent = "Hello World ";
+newElement.className = "Header";
+
+document.getElementById("root").append(newElement);
