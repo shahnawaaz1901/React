05cbe1d29961862625b83a7b055e7a6a085a4af5
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-19 20:33:37 +0530

    Understand JSX and Requirement of JSX

---
+++ 00-Introduction_to_React/08-Understand_JSX.js
@@ -0,0 +1,19 @@
+/*
+    JSX : JSX Stands for Javascript XML. JSX is the extension which provides 
+        us more convinient way to write code in React, We need to understand 
+        that it's just a syntactic sugar internaly working should be same as 
+        previous because again and again createElement using React it's hard 
+        to write long syntax like :
+
+        React.createElement("div",{className : "header"},"This is Heading");
+
+        JSX Provide us more convinient way to write code in react instead of like 
+        this we can write code same as "HTML", because JSX just combination of 
+        HTML and Javascript but browser not support any other languages except
+        HTML, CSS and Javascript so question is how can browser understand this
+        JSX file, for this many compiler is exist which manage JSX file and
+        converts this JSX file into seprate HTML and Javascript so browser is
+        able to understand that JSX format file.
+
+        Compiler which we used for is "Babel".
+*/
