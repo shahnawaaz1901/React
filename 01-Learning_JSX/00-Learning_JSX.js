07da12716c5f8d30bb14d902a81b2a42da1a9925
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-21 20:56:27 +0530

    Start to Learn More About JSX

---
+++ 01-Learning_JSX/00-Learning_JSX.js
@@ -0,0 +1,19 @@
+/*
+    JSX : JSX Stands for "Javascript XML", which is nothing but the 
+        extension of Javascript Syntax. Which Provides react to be
+        declarative. JSX is just the syntactic suger over the react
+        Code which is written in Javascript.
+
+*/
+
+/*
+    Why JSX : 
+    - Because using JSX, React embrace the fact that "rendering logic and 
+        UI logic" we can put together
+    - JSX work as "visual aid" while working wit UI inside Javascript.
+    - React "Seprate Concerns" means with react we can devide multiple things 
+        of UI into multiple components But in normal HTML & Javascript we 
+        only seprate both the technology into multiple files but then we
+        are not seprate each and every area of UI
+    - JSX Allows react to show more "Useful Errors" and as Warning Messages
+*/
---
+++ 01-Learning_JSX/01-What_is_the_Output.js
@@ -0,0 +1,10 @@
+/*
+Advantages of JSX
+
+What are the advantages of using JSX in a React app? (Multiple)
+
+It helps to couple rendering logic with the UI logic            //* Correct
+It Seprates concerns instead of seprating technologies          //* Correct
+It Helps to decoupple rendering logic from the UI logic         
+It Helps react to show more useful error and warning messages   //* Correct
+*/
