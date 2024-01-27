a54654a3f98a8ac8439471c98c601cb3ddba1603
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-28 01:26:08 +0530

    Install React in the System and start new Project with React

---
+++ 03-Create_react_app/03-Create_React_App.js
@@ -0,0 +1,25 @@
+/*
+    For Starting the Project we need to install create-react-app globally so 
+    whenever we want to start new Project we start using the create-react-app 
+    and the name of the project, so for install the create-react-app globally
+    we need to give few command :
+
+        npm i -g create-react-app
+    
+    -g means we want to install globally
+
+    After installing the create-react-app globally, if we want to start new react 
+    project then we can start using command :
+    create-react-app project_name 
+    then a folder with the projectname is created and some prewritten code is there 
+    in src and public folder and as well as package.json and package-lock.json
+
+    One thing that we need to remember that projectname is always in small letter
+    any capital letter cause of an error in terminal
+
+    if we are not installed the create-react-app globally and still want to create
+    a project then we create project by just writing an keyword npx at the start,
+    just like this command :
+
+    npx create-react-app projectname
+*/
