0b63d140aca7c218901085a2fc744dc4f260046d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-27 21:33:17 +0530

    Understand DrawBack of CDNs

---
+++ 03-Create_react_app/00-Problems_with_CDN.js
@@ -0,0 +1,17 @@
+/*
+    CDN : CDN Stands for "Content Delivery Network", It's Helps us to use 
+        third party libraries over the internet without installing the 
+        libraries or package on the system. This Helps a low end systems
+        which is not capable to manage the libraries.
+*/
+
+/*
+    DrawBack of CDNs : 
+        - Using CDN we will Only able to get the Actual result when we connected 
+            over the internet without Internet it's not give you desired result
+        - Everytime we use some file of function which is coming from CDN then 
+            our system sends request to the server and server will respond by
+            the result, So it cause the delay of loading the page or suppose if
+            server is overloaded and can't handle more request then server will
+            not respond and our program will stop to run
+*/
