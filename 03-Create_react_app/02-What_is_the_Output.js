1ae061fd5062392d393391b08ec562bbc0f333f0
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-27 22:36:54 +0530

    MCQs question around CDNs and Webpack

---
+++ 03-Create_react_app/02-What_is_the_Output.js
@@ -0,0 +1,49 @@
+/*
+Disadvantages of CDNs
+Which are the disadvantages of using a CDN .?(Multiple)
+
+1. CDN does not work withOut an internet Connection.    //* Correct
+2. CDN can fail if the servers running that service     //* Correct
+    fails to operate.   
+3. CDN can cause support and infrastructure issues      //* Correct
+4. CDN has less functionality that using a local setup with webpack 
+*/
+
+/*
+Scripts in HTML
+Which of the following statements are true about adding scripts to HTML.?(Multiple)
+
+1. Script are loaded synchronously from top to bottom.      //* Correct
+2. Scripts are loaded asynchronously
+3. Scripts at the top do not have access to other scripts   //* Correct
+    below them.       
+4. Scripts can only be added to the head tag in HTML.
+*/
+
+/*
+WebPack Configuration
+What is the entry property in the Webpack configuration file.?
+
+1. It Specifies the output directory for the bundled modules
+2. It indicates which files will be at the bottom of its 
+    internal dependency graph.
+3. It indicates which files will be at the top its internal     //* Correct
+    dependency graph.           
+4. It tells webpacks how to process package.json file and convert 
+    it into valid modules
+
+*/
+
+/*
+Loaders in Webpack
+What is the use of loaders in webpack .?
+
+1. Loaders allows webpack to load different type of files to 
+    the browser
+2. Loaders allows webpack to transform files into valid modules.        //* Correct
+3. Loaders allows webpack to transform all the files into 
+    javascript code.
+4. Loaders allows webpack to transform all the files in th html 
+    code.
+
+*/
