8882a2dcabf7c90afb2db028b387a850a08911f3
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-28 19:00:32 +0530

    understand folder structure of create-react-app

---
+++ 03-Create_react_app/04-Understand_Folder_Structure_of_React_Proejct.js
@@ -0,0 +1,49 @@
+/*
+    
+    Installation : Every Library we Require in our project we can use it by 
+        installing the Library using the command "npm install", but we can 
+        install library by three types :
+
+        Local Installation :
+        - npm i package_name
+
+        Global Installation : 
+        - npm i -g package_name
+
+        Dev Dependencies :
+        - npm i --save-dev package_name
+
+
+    All the Dependencies or library files is installed in node_modules folder 
+    that's why we dont need to commit this folder to the Github, because we can
+    install the library usign the npm i command and whole dependencies of the 
+    project is started to install, then there are two folders src and public
+    where src contains the server files which helps us to run our front end
+    project over the server on localhost and client contains the static files
+    which contains the front end part of our project which we want to build
+    and in public folder one file index.html has which is the main file of our
+    project
+*/
+
+/*
+    manifest.json : This files contains the information and description of our 
+        app, in this file all icons and images files information is stored
+*/
+
+/*
+    src folder main file is index.js because index.js is main server file which is
+    responsible to host our frontend project to the localhost, except this we can
+    modify or change the all the files
+*/
+
+/*
+    package.json : package.json contains the information of all thirdparty libraries 
+        and dependencies which we use and install in our project along with 
+        information of some other scripts, tesing and injecting files
+*/
+
+/*
+    package-lock.json : package-lock.json contains more information regarding the 
+        dependencies including the version and more related information regarding
+        the package
+*/
