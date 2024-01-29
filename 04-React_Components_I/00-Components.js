6b87f0c4e107b2621558545d50040e1d3e133d41
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-29 20:00:47 +0530

    Understand Class components

---
+++ 04-React_Components_I/00-Components.js
@@ -0,0 +1,14 @@
+/*
+    We Know that Components in React are two types :
+    - Functional Components
+    - Class Components
+*/
+
+/*
+    Class Component is Some Different from functional Component so first if 
+    we need to create component from class we need to inherit properties of 
+    component from React by using the extends React.component.
+
+    In Class Component a render function will be need to present which returns
+    some JSX and that is render on to the screen.
+*/
