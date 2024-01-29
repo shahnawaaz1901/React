69ddb5f680f94374294431a0efcff042fe8782b7
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-29 20:09:46 +0530

    Create a Class Component in React

---
+++ 04-React_Components_I/classcomponent/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//* Create App Component using classes by inherit properties from React.component
+export default class App extends React.Component {
+  //* Render function is default method which returns JSX and Render on to the Browser
+  render() {
+    return (
+      <>
+        <h1>This is React Component</h1>
+      </>
+    );
+  }
+}
