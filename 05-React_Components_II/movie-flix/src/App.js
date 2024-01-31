9672915e28662aa16dea6d8249171fdf1dfd3898
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-31 18:25:24 +0530

    Start to Add new Features on Movie App

---
+++ 05-React_Components_II/movie-flix/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieCard from "./movieCard.js";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <MovieCard />
+      </>
+    );
+  }
+}
---
+++ 05-React_Components_II/movie-flix/src/movieCard.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./styles.css";
+
+export default class MovieCard extends React.Component {
+  //* Define the Constructor for using the State
+  constructor() {
+    super();
+    /* Please remember that state is the object */
+    this.state = {
+      title: "The Avengers",
+      image:
+        "https://m.media-amazon.com/images/W/MEDIAX_849526-T1/images/I/516FQSpAR8L._SX300_SY300_QL70_FMwebp_.jpg",
+      plot: "Super Natural Power Shown in the Movie",
+      price: 99,
+      ratings: "8.9",
+      stars: 0,
+    };
+  }
+
+  increaseStars() {
+    if (this.state.stars >= 5) {
+      return;
+    }
+
+    this.setState((prevState) => {
+      return {
+        stars: prevState.stars + 0.5,
+      };
+    });
+  }
+
+  decreaseStars() {
+    if (this.state.stars <= 0) {
+      return;
+    }
+    //* Second form of setState function
+    this.setState((prevState) => {
+      return {
+        stars: prevState.stars - 0.5,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div className="movie-card">
+          <div className="left">
+            <img alt="MovieLogo" src={this.state.image} />
+          </div>
+          <div className="right">
+            <div className="title">{this.state.title}</div>
+            <div className="plot">{this.state.plot}</div>
+            <div className="price">{this.state.price}/- INR</div>
+            <div className="footer">
+              <div className="rating">{this.state.ratings}/10</div>
+              <div className="star-dis">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/9210/9210950.png"
+                  alt="decrease"
+                  className="stars"
+                  onClick={this.decreaseStars.bind(this)}
+                />
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/10134/10134048.png"
+                  alt="star"
+                  className="stars"
+                />
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/8162/8162972.png"
+                  alt="increase"
+                  className="stars"
+                  onClick={this.increaseStars.bind(this)}
+                />
+                <span className="starCount">{this.state.stars}</span>
+              </div>
+              <button className="favourite-btn">Favourite</button>
+              <button className="cart-bt">Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
