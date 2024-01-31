2b7ead077c0ca72198a9b92037baa9055df95094
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-01 01:41:54 +0530

    understand more about props and how to collect props in function component

--- 05-React_Components_II/student/src/student.js
+++ 05-React_Components_II/student/src/student.js
@@ -1,5 +1,6 @@
-import { Component } from "react";
+// import { Component } from "react";
 
+/*
 export default class Student extends Component {
   constructor() {
     super();
@@ -11,6 +12,7 @@ export default class Student extends Component {
 
   render() {
     // return this.set.bind(this)("ashu");
+    console.log(this.props);
     return (
       <>
         <h1>Name : {this.props.name}</h1>
@@ -18,16 +20,30 @@ export default class Student extends Component {
       </>
     );
   }
+}
+*/
 
-  set = (data) => {
-    console.log(data);
-    this.setState(data, () => {
-      return (
-        <>
-          <h1>Name : {this.state.name}</h1>
-          <h2>Marks : {this.state.marks}</h2>
-        </>
-      );
-    });
-  };
+/* 
+    Because we dont have any this keyword value in the functions so instead 
+    of "this" keyword props we recieve in the argument 
+*/
+
+/*
+    One thing that we need to remember that is we can't modify the props
+    we can read the props in child Component but can not update the props 
+    from the child component this permission is only allows for the parent
+*/
+export default function Student(props) {
+  /*
+    Thrown error can't assign value to read only property, because we can't 
+    modify the props however we can modify the states using the setStates function
+    props.name = "Ashu";
+  */
+  console.log(props);
+  return (
+    <>
+      <h1>Name : {props.name}</h1>
+      <h2>Marks : {props.marks}</h2>
+    </>
+  );
 }
--- 05-React_Components_II/student/src/studentList.js
+++ 05-React_Components_II/student/src/studentList.js
@@ -13,8 +13,24 @@ export default class StudentList extends Component {
         <Student name="Ashu Ansari" marks={90} />
         <Student name="Sadaf Noor" marks={85} />
         <Student name="Toshiba Ansari" marks={91} />
-        <Student name="Farheen Ansari" marks={93} />
+        <Student />
       </>
     );
   }
 }
+
+//* Setting Student Component defaultProps Where Student is Class Component
+Student.defaultProps = {
+  name: "Student",
+  marks: "N/A",
+};
+
+/* 
+    Suppose we Just forget to pass the somedata on props so value of that 
+    key in the component is undefined and we wouldn't be able to render
+    the undefined on the browser, so for prevent this condition we can just
+    pass the default props which is over riden the undefined value.
+
+    We can set default props by just setting the defaultProps properties in
+    the component which we want to set the Property
+*/
