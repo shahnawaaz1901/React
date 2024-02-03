817f6534874a57ba17aace91f8891ef8ddd0a866
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-03 22:41:51 +0530

    Understand andTried to Implement Styled Component

--- 06-Styling_in_React/movie-flix/src/Navbar.js
+++ 06-Styling_in_React/movie-flix/src/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import styled from "styled-components";
 /*
 Inline Styling is the makes code very messi and Lengthy so instead of passing
 object like this we can create seprate object of styling called styles and
 inside style write many classes which is used for every object so we can say
 we are create a object which contains styling of multiple elements
-*/
+
 const styles = {
   nav: {
     width: "100%",
@@ -43,7 +43,7 @@ const styles = {
     fontSize: "12px",
   },
 };
-/* 
+ 
   Now Question is if we use external css and include that css file in the 
   main App component then why it's required to use the inline css because 
   index.css or whatever css file we created externally it's have the global
@@ -54,6 +54,19 @@ const styles = {
   so pretty also we are not using the inline css but we use style component
   and css modules which provides more better way to us to writing the style
 */
+
+//* Add Styling using the Styled Component
+/* 
+  For Writing Styles using the styled component we need to use style keyword 
+*/
+const Nav = styled.nav`    width: 100%,
+background-color: rgb(20, 20, 96),
+color: white,
+height: 70px,
+display: flex,
+justify-content: space-between,
+padding: 10px 30px,
+box-sizing: border-box,`;
 export default class Navbar extends React.Component {
   render() {
     return (
@@ -71,17 +84,17 @@ export default class Navbar extends React.Component {
         and also it's messy to write also we can't use the same type of class
         for multiple elements
     */
-      <nav style={styles.nav}>
+      <Nav>
         <div>Movie Flix</div>
-        <div style={styles.cartContainer}>
+        {/* <div style={styles.cartContainer}>
           <img
             src="https://cdn-icons-png.flaticon.com/128/4290/4290854.png"
             alt="Cart-Icon"
             style={styles.cartIcon}
           />
-          <sup style={styles.cartCount}>0</sup>
-        </div>
-      </nav>
+          <span style={styles.cartCount}>0</span>
+        </div> */}
+      </Nav>
     );
   }
 }
