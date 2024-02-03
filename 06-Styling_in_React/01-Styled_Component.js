9bb418294261be6948080499a0968ab4e3d20416
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-03 22:12:51 +0530

    Introduction to Styled Components

---
+++ 06-Styling_in_React/01-Styled_Component.js
@@ -0,0 +1,8 @@
+/*
+    Styled Component : Styled Component is nothing but a third party library 
+        which helps us to write css in javascript, this eleminated the many 
+        problem like normal css file has the global scope but the styled 
+        component is has the local scope, so in this we can style a single
+        component and use that component easily, another benefits is there is
+        no clashes between component classes because of the local scope
+*/
