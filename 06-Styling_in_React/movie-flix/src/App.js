6e73d5f50b1ee863a727a67f89838f061c92adef
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-05 19:17:24 +0530

    Completed the Movie List App Project

--- 06-Styling_in_React/movie-flix/src/App.js
+++ 06-Styling_in_React/movie-flix/src/App.js
@@ -11,6 +11,10 @@ export default class App extends React.Component {
         Movies Array is Bulky so we transfer all movies array data from 
         this app to seprate movieData file
       */
+      /*
+        Prop Drilling : Passing State as props from one parent to child as prop and pass 
+                that props to that component child this is called prop drilling. 
+      */
       data: movies,
       cartCount: 0,
     };
@@ -46,15 +50,24 @@ export default class App extends React.Component {
   };
 
   handleCart = (movie) => {
+    let cartCount = this.state.cartCount;
     const index = this.state.data.indexOf(movie);
     const { data: movies } = this.state;
     movies[index].isInCart = !movies[index].isInCart;
-    this.setState({ data: movies });
+    //* One Method to Update State
+    if (movies[index].isInCart) {
+      cartCount += 1;
+    } else {
+      cartCount -= 1;
+    }
+    /* Another Method
     if (movies[index].isInCart) {
       this.changeCart("increase");
     } else {
       this.changeCart("decrease");
     }
+    */
+    this.setState({ data: movies, cartCount });
   };
 
   changeCart(work) {
