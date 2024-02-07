2d43c2841ab8a08f0df97ae861b63ef06e16f78f
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-07 20:20:43 +0530

    Understand LifeCycle in React and Components

---
+++ 07-Component_LifeCycle/00-Component_Lifecycle.js
@@ -0,0 +1,46 @@
+/*
+    Two Types of Components Exist in React : 
+    1. Functional Component
+    2. Class Component
+
+    Component has their LifeCycle.
+    Now Question is what is Lifecycle.
+
+    LifeCycle : A LifeCycle is the series of stages through which a living 
+            thing passes from the beggining of its life until its death.
+
+    A Component lifecycle is replaced from living thing to component in case
+    of Component Lifecycle.
+
+    When a Component is used in the Another component its says that component 
+    is created or we can say when component is used in DOM in technical term
+    this is called the Mounted, and when a Component is removed from the DOM
+    this is called death of component means lifecycle is end for this component
+    in the technical term it's unmounted, Different Phases of Component life 
+    cycle is exist
+*/
+
+/*
+    Different Phases of Life Cycle :
+
+    1. Mounting : When Component is Created and Put inside the DOM
+    2. Updating : When Comonent is Updated
+    3. UnMounting : When Component is Removed from the DOM
+    4. Error Handling : When Error is occure During Rendering of Component
+*/
+
+/*
+    Side Effects : SideEffects are nothing but unpredictable actions because 
+            they are performed with the "outside world".
+
+    When we call the Browser API then it'll be consider as the side effect 
+    because browser API is external to React not the Internal to the React
+    including the native DOM methods instead of native react DOM methods it
+    is also called the side effect because it's not consider in the React but
+    React DOM method is also a side effect.
+
+    If we fetching the Data through API it's also considered the side effect 
+    and if we also subscribe or call any database it's also side effect including
+    react we perform multiple side in some place
+
+*/
