4283c3e859c6cedc5d99f58b7933795747e4d7e5
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-16 16:01:09 +0530

    Started the Project

--- 08-Mini_Project/ipod/src/components/IPod.js
+++ 08-Mini_Project/ipod/src/components/IPod.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Display from "./Display";
 import Wheel from "./Wheel";
+import ipod from "./ipod.module.css";
 
 class IPod extends React.Component {
   render() {
     return (
-      <>
+      <div className={ipod.container}>
         <Display />
         <Wheel />
-      </>
+      </div>
     );
   }
 }
