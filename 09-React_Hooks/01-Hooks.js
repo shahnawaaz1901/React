9451becb37fd72aa3d9eb696142cfb720f9c7a4c
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-23 19:15:23 +0530

    Understand What are Hooks and Start new Project usng Hooks

---
+++ 09-React_Hooks/01-Hooks.js
@@ -0,0 +1,13 @@
+/*
+    Hooks : Hooks are nothing but Functions that let you hook into React State 
+        and lifeCycle Features from functional Component. Hooks are introduced
+        in React v16.8
+
+    As we learn so far React has state and lifecycle methods but we can't access 
+    that lifecycle methods into the functional component but with Using hooks we
+    can use states and all the lifecycle methods( which we learnt for Class based
+    components ) into the functional Component.
+
+    Note : Hooks can't be used in class based components because class based 
+        components already have the state and lifecycle methods.
+*/
