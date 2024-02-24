f0d2ce247bf2445cecb006ddfea70e1326f982b7
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-25 01:00:31 +0530

    Start to Add More Features to the Blog Project

---
+++ 09-React_Hooks/blogproject/src/Components/Blog.js
@@ -0,0 +1,74 @@
+//Blogging App using Hooks
+import { useState } from "react";
+export default function Blog() {
+  //* Set State
+  const [blogs, updateBlogs] = useState([]);
+
+  //Passing the synthetic event as argument to stop refreshing the page on submit
+  function handleSubmit(e) {
+    e.preventDefault();
+    let title = e.target[0].value;
+    let content = e.target[1].value;
+    e.target[0].value = "";
+    e.target[1].value = "";
+    const blogData = {
+      title,
+      content,
+    };
+    console.log(blogs);
+    blogs.push(blogData);
+    console.log(blogs);
+    updateBlogs({ blogs });
+  }
+
+  return (
+    <>
+      {/* Heading of the page */}
+      <h1>Write a Blog!</h1>
+      {/* Division created to provide styling of section to the form */}
+      <div className="section">
+        {/* Form for to write the blog */}
+        <form onSubmit={handleSubmit}>
+          {/* Row component to create a row for first input field */}
+          <Row label="Title">
+            <input
+              className="input"
+              placeholder="Enter the Title of the Blog here.."
+            />
+          </Row>
+
+          {/* Row component to create a row for Text area field */}
+          <Row label="Content">
+            <textarea
+              className="input content"
+              placeholder="Content of the Blog goes here.."
+            />
+          </Row>
+
+          {/* Button to submit the blog */}
+          <button className="btn">ADD</button>
+        </form>
+      </div>
+
+      <hr />
+
+      {/* Section where submitted blogs will be displayed */}
+      <h2> Blogs </h2>
+    </>
+  );
+}
+
+//Row component to introduce a new row section in the form
+function Row(props) {
+  const { label } = props;
+  return (
+    <>
+      <label>
+        {label}
+        <br />
+      </label>
+      {props.children}
+      <hr />
+    </>
+  );
+}
