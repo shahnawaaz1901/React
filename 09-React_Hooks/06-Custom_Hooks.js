8cb978b8308276e6be41d21b98725bec66c57e4d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-28 21:32:04 +0530

    Start to understand custom hooks with basic project

---
+++ 09-React_Hooks/06-Custom_Hooks.js
@@ -0,0 +1,16 @@
+/*
+    Custom Hooks : React Provides us the functionalities to Design and 
+        create our own hooks called custom hooks.
+
+    Suppose we have a login like in a ecom app amazon, in that application
+    we enter our email and we forgot the password then we click on password
+    reset form and when we click on that form a forgot password form is 
+    render on the screen which already contains our email address but now 
+    question is where from this email comes this means our email is stored
+    somewhere so that amazon webpage takes my email from there, this is done
+    by using the custom hooks because customs hooks provides us to prevent
+    the repetative tasks and gives the functionality to pass the stateful 
+    logic between multiple components, however we can do this withOut the
+    custom hooks but custom hooks provides us to do same task withOut doing
+    the same repetative task
+*/
