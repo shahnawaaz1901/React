5403d30c5e76dd933701439be8fb72d68fb43f3c
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-03-02 14:02:29 +0530

    Added Some minor changes

--- 08-Mini_Project/ipod/src/components/Music_Components/Player.js
+++ 08-Mini_Project/ipod/src/components/Music_Components/Player.js
@@ -6,50 +6,6 @@ export default class MusicPlayer extends React.Component {
     this.state = {
       currentTime: 0,
       currentSong: null,
-      songs: [
-        {
-          name: "Baby",
-          singer: "Justin Bieber, Ludacris",
-          poster: "https://pagalworlld.com/siteuploads/thumb/sft10/4702_4.jpg",
-          src: "https://pagalworlld.com/files/download/id/4702",
-          duration: 220,
-          state: "play",
-        },
-        {
-          name: "Heart Attack",
-          singer: "Enrique Iglesias",
-          poster: "https://pagalworlld.com/siteuploads/thumb/sft25/12369_4.jpg",
-          src: "https://pagalworlld.com/files/download/id/12369",
-          duration: 231,
-          state: "play",
-        },
-        {
-          name: "Love Story",
-          singer: "Taylor Swift",
-          poster: "https://pagalworlld.com/siteuploads/thumb/sft7/3126_4.jpg",
-          src: "https://pagalworlld.com/files/download/id/3126",
-          duration: 237,
-          state: "play",
-        },
-        {
-          name: "Despacito",
-          singer: "Luis Fonsi, Daddy Yankee",
-          poster:
-            "https://www.pagalworld.com.cm/siteuploads/thumb/sft6/2858_4.jpg",
-          src: "https://www.pagalworld.com.cm/siteuploads/files/sfd6/2858/Despacito(PagalWorld.com.cm).mp3",
-          state: "play",
-          duration: 281,
-        },
-        {
-          name: "Into Your Arms",
-          singer: "Witt Lowry",
-          poster:
-            "https://pagallworld.co.in/wp-content/uploads/2023/08/Into-Your-Arms-Slowed-Reverb.jpg",
-          src: "https://pagallworld.co.in/wp-content/uploads/2023/08/Into-Your-Arms-Slowed-Reverb.mp3",
-          state: "play",
-          duration: 193,
-        },
-      ],
     };
   }
 
@@ -61,6 +17,7 @@ export default class MusicPlayer extends React.Component {
   }
 
   componentDidMount() {
+    console.log(document.getElementById("audio"));
     this.timer = setInterval(() => {
       if (this.state.currentTime === this.state.maxTime) {
         clearInterval(this.timer);
@@ -78,7 +35,6 @@ export default class MusicPlayer extends React.Component {
   }
 
   render() {
-    console.log(this.props.nextSong);
     const { currentSong } = this.state;
     const width = (this.state.currentTime / currentSong.duration) * 100 + "%";
     return (
@@ -91,14 +47,14 @@ export default class MusicPlayer extends React.Component {
             <div className={styles.musicDetail}>
               <div className={styles.song}>{currentSong.name}</div>
               <div className={styles.singer}>{currentSong.singer}</div>
+              <audio id="audio">
+                <source
+                  src={currentSong.src}
+                  // "https://pagallworld.co.in/wp-content/uploads/2023/06/Enrique-Iglesias-Heart-Attack.mp3"
+                  type="audio/mpeg"
+                />
+              </audio>
             </div>
-            <audio autoPlay hidden>
-              <source
-                src={currentSong.src}
-                // "https://pagallworld.co.in/wp-content/uploads/2023/06/Enrique-Iglesias-Heart-Attack.mp3"
-                type="audio/mpeg"
-              />
-            </audio>
           </div>
           <div className={styles.musicRun}>
             <div className={styles.meter}>
