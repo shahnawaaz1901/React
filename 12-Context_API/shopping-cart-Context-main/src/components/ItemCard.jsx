2faa8015c2983f38507b69a849d149d482f26889
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-03-25 12:24:07 +0530

    Implement the Custom Hook and Write Custom Component for convinience

--- 12-Context_API/shopping-cart-Context-main/src/components/ItemCard.jsx
+++ 12-Context_API/shopping-cart-Context-main/src/components/ItemCard.jsx
@@ -1,39 +1,40 @@
 import React from "react";
 import styles from "../styles/ItemCard.module.css";
-import { useContext } from "react";
-import { ItemContext } from "../contexts/item.context";
+// import { useContext } from "react";
+// import { ItemContext } from "../contexts/item.context";
+import { useValue } from "../contexts/item.context";
 
 function ItemCard({ name, price }) {
-  const {total, setTotal,item, setItem} = useContext(ItemContext);
-  
+  const {handleAdd, handleRemove} = useValue();
+  /* Moving All the Handle Ful Logic inside the Context file
   const handleAdd = () => {
     setTotal(total + price);
     setItem(item + 1);
   };
 
   const handleRemove = () => {
-    /* 
+     
       setTotal also written in the two form one is directly pass the value 
       and another one is by a callback function which takes state as argument
       and whatEver value we return that value is set to the state
     if(total){
       setTotal(total - price)
     }
-    */
+    
 
     setTotal((prevState)=> prevState <= 0 ? prevState : prevState - price)
     setItem((prevState)=> prevState <= 0 ? prevState : prevState - 1);
   };
-
+*/
   return (
     <div className={styles.itemCard}>
       <div className={styles.itemName}>{name}</div>
       <div className={styles.itemPrice}>&#x20B9; {price}</div>
       <div className={styles.itemButtonsWrapper}>
-        <button className={styles.itemButton} onClick={() => handleAdd()}>
+        <button className={styles.itemButton} onClick={() => handleAdd(price)}>
           Add
         </button>
-        <button className={styles.itemButton} onClick={() => handleRemove()}>
+        <button className={styles.itemButton} onClick={() => handleRemove(price)}>
           Remove
         </button>
       </div>
--- 12-Context_API/shopping-cart-Context-main/src/contexts/item.context.js
+++ 12-Context_API/shopping-cart-Context-main/src/contexts/item.context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 export const ItemContext = createContext();
 
 /*
@@ -8,6 +8,26 @@ export const ItemContext = createContext();
 function CustomProvider(props) {
   const [total, setTotal] = useState(0);
   const [item, setItem] = useState(0);
+
+  const handleAdd = (price) => {
+    setTotal(total + price);
+    setItem(item + 1);
+  };
+
+  const handleRemove = (price) => {
+    /* 
+      setTotal also written in the two form one is directly pass the value 
+      and another one is by a callback function which takes state as argument
+      and whatEver value we return that value is set to the state
+    if(total){
+      setTotal(total - price)
+    }
+    */
+
+    setTotal((prevState) => (prevState <= 0 ? prevState : prevState - price));
+    setItem((prevState) => (prevState <= 0 ? prevState : prevState - 1));
+  };
+
   /*
     props which we are getting inside the argument that's nothing but the all
     the list of children which are wrap inside our this CustomProvider component
@@ -15,8 +35,14 @@ function CustomProvider(props) {
     children of the component, or we can say all the content between the component
     can be accessed by the props.children
   */
+  /* 
+    Because we write handleAdd and handleRemove inside this context file we dont 
+    need to pass both the function inside the object in value attribute 
+  */
   return (
-    <ItemContext.Provider value={{ total, setTotal, item, setItem }}>
+    <ItemContext.Provider
+      value={{ total, setTotal, item, setItem, handleAdd, handleRemove }}
+    >
       {/*
         Context.Provider is a default provider which helps us to provide the 
         whatEver values we want to pass to the Children Components
@@ -32,4 +58,9 @@ function CustomProvider(props) {
   );
 }
 
+function useValue() {
+  return useContext(ItemContext);
+}
+
+export { useValue };
 export default CustomProvider;
