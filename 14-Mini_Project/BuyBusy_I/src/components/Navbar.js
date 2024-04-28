28e72a672464af35c636cf6cce2a63520ed8a838
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-04-28 20:33:31 +0530

    Downloaded necessory images and start to implement navbar

--- 14-Mini_Project/BuyBusy_I/src/components/Navbar.js
+++ 14-Mini_Project/BuyBusy_I/src/components/Navbar.js
@@ -1,2 +1,18 @@
-function Navbar() {}
+import styles from "./navbar.module.css";
+import { NavLink } from "react-router-dom";
+function Navbar() {
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.start}>
+        <NavLink to="/">Buy Busy</NavLink>
+      </div>
+      <div className={styles.end}>
+        <div className={styles.navItem}>Home</div>
+        <div className={styles.navItem}>My Orders</div>
+        <div className={styles.navItem}>Cart</div>
+        <div className={styles.navItem}>Login</div>
+      </div>
+    </div>
+  );
+}
 export default Navbar;
