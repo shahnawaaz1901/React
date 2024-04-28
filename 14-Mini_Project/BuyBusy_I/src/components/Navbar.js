c3335b348d56f6346ecacf8acc2ecd392b0757a7
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-04-28 21:05:57 +0530

    Added More Styles to the navbar

--- 14-Mini_Project/BuyBusy_I/src/components/Navbar.js
+++ 14-Mini_Project/BuyBusy_I/src/components/Navbar.js
@@ -1,4 +1,8 @@
 import styles from "./navbar.module.css";
+import cart from "../data/download.png";
+import order from "../data/download (1).png";
+import user from "../data/download (2).png";
+import home from "../data/download (3).png";
 import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
@@ -7,10 +11,38 @@ function Navbar() {
         <NavLink to="/">Buy Busy</NavLink>
       </div>
       <div className={styles.end}>
-        <div className={styles.navItem}>Home</div>
-        <div className={styles.navItem}>My Orders</div>
-        <div className={styles.navItem}>Cart</div>
-        <div className={styles.navItem}>Login</div>
+        <div className={styles.navItem}>
+          <NavLink to="home">
+            <div className={styles.logo}>
+              <img src={home} alt="home" />
+            </div>
+            <div>Home</div>
+          </NavLink>
+        </div>
+        <div className={styles.navItem}>
+          <NavLink to="orders">
+            <div className={styles.logo}>
+              <img src={order} alt="order" />
+            </div>
+            <div>My Orders</div>
+          </NavLink>
+        </div>
+        <div className={styles.navItem}>
+          <NavLink to="cart">
+            <div className={styles.logo}>
+              <img src={cart} alt="cart" />
+            </div>
+            <div>Cart</div>
+          </NavLink>
+        </div>
+        <div className={styles.navItem}>
+          <NavLink to="user">
+            <div className={styles.logo}>
+              <img src={user} alt="user" />
+            </div>
+            <div>Signin</div>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
