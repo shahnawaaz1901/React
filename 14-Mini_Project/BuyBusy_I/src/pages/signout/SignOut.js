a642941585338594f9c717ccd2d114deba48eea7
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-05-13 22:05:27 +0530

    Implement the Signout Route

--- 14-Mini_Project/BuyBusy_I/src/pages/signout/SignOut.js
+++ 14-Mini_Project/BuyBusy_I/src/pages/signout/SignOut.js
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
+
 function SignOut() {
   const navigate = useNavigate();
-  localStorage.setItem("userInfo", null);
+  localStorage.setItem("userInfo", "");
   navigate("/");
 }
+
 export default SignOut;
