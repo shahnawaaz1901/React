f025f15141bfc763d0ff2310d52140e0c206673a
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-05-18 17:31:02 +0530

    Created the Store

--- 16-Redux_with_React/To-Do Project using Redux/src/redux/store.js
+++ 16-Redux_with_React/To-Do Project using Redux/src/redux/store.js
@@ -1,9 +1,16 @@
 import todoReducer from "./reducers/todoReducer";
 import redux from "redux";
 
-/* 
+/*
     Create the Store using createStore which is Deprecated we Learn Latest 
     configureStore React Toolkit in upcoming videos
 */
+
+/*
+    We are creating the store file a single file because it's good practice
+    to create a single file for store, not recommend the folder until it's
+    required 
+*/
+
 const store = redux.createStore(todoReducer);
 export default store;
